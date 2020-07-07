e51d662b340cd58098e2688356dc55c5037db843
Ian Sweet <iannsweet@gmail.com>
2020-07-07 16:29:14 -0400

    update source ast to generate better documentation (and formatting)

--- src/source.ml
+++ src/source.ml
@@ -3,86 +3,105 @@ open Stdio
 
 exception SyntaxError of Position.t * String.t
 
-type t = { source_location  : Section.t
-         ; datum : t'
-         }
+type t = {
+  source_location  : Section.t;
+  datum : t'
+}
 
 and t' =
-  | ELit      of { datum  : Literal.t (** Literal *)
-                 ; label  : Label.t
-                 }
+  | ELit of {
+      datum : Literal.t;
+      label : Label.t
+    } (** Literal *)
 
-  | EFlip     of Region.t (** Random Boolean *)
+  | EFlip of Region.t (** Random Boolean *)
 
-  | ERnd      of Region.t (** Random Integer *)
+  | ERnd of Region.t (** Random Integer *)
 
-  | EVar      of Var.t List.t (** Variable *)
+  | EVar of Var.t List.t (** Variable *)
 
-  | EBOp    of { op  : Boolean.Op.t (** Boolean Operation *)
-               ; args : t List.t
-               }
+  | EBOp of {
+      op : Boolean.Op.t;
+      args : t List.t
+    } (** Boolean Operation *)
 
-  | EAOp    of { op  : Arith.Op.t (** Arithmetic Operation *)
-               ; args : t List.t
-               }
+  | EAOp of {
+      op : Arith.Op.t;
+      args : t List.t
+    } (** Arithmetic Operation *)
 
-  | EARel   of { rel : Arith.Rel.t (** Arithmetic Relation *)
-                 ; args : t List.t
-                 }
+  | EARel of {
+      rel : Arith.Rel.t;
+      args : t List.t
+    } (** Arithmetic Relation *)
 
-  | ETuple    of (t, t) Tuple.T2.t   (** Tuple *)
+  | ETuple of (t, t) Tuple.T2.t (** Tuple *)
 
-  | ERecord   of (Var.t * t) List.t   (** Record *)
+  | ERecord of (Var.t * t) List.t (** Record *)
 
-  | EArrInit  of { size : t   (** Array Initialization *)
-                 ; init : t
-                 }
+  | EArrInit of {
+      size : t;
+      init : t
+    } (** Array Initialization *)
 
-  | EArrRead  of { loc : t   (** Array Read *)
-                 ; idx  : t
-                 }
+  | EArrRead of {
+      loc : t;
+      idx : t
+    } (** Array Read *)
 
 
-  | EArrWrite of { loc  : t   (** Array Write *)
-                 ; idx   : t
-                 ; value : t
-                 }
+  | EArrWrite of {
+      loc : t;
+      idx : t;
+      value : t
+    } (** Array Write *)
 
-  | EArrLen   of t   (** Array Length *)
+  | EArrLen of t (** Array Length *)
 
-  | ECast     of { var : Var.t ; label : Label.t }
+  | ECast of {
+      var : Var.t;
+      label : Label.t
+    } (** Cast *)
 
-  | EMux      of { guard : t   (** Mux *)
-                 ; lhs   : t
-                 ; rhs   : t
-                 }
+  | EMux of {
+      guard : t;
+      lhs : t;
+      rhs : t
+    } (** Mux *)
 
-  | EAbs      of { pat : Pattern.t   (** Abstraction *)
-                 ; body  : t
-                 }
+  | EAbs of {
+      pat : Pattern.t;
+      body : t
+    } (** Abstraction *)
 
-  | ERec      of { name  : Var.t   (** Recursive Abstraction *)
-                 ; pat : Pattern.t
-                 ; body  : t
-                 ; t_ret : Type.t
-                 }
+  | ERec of {
+      name : Var.t;
+      pat : Pattern.t;
+      body : t;
+      t_ret : Type.t
+    } (** Recursive Abstraction *)
 
-  | EApp      of { lam : t   (** Application *)
-                 ; arg : t
-                 }
+  | EApp of {
+      lam : t;
+      arg : t
+    } (** Application *)
 
-  | ELet      of { pat   : Pattern.t   (** Let-Binding *)
-                 ; value : t
-                 ; body  : t
-                 }
+  | ELet of {
+      pat : Pattern.t;
+      value : t;
+      body : t
+    } (** Let-Binding *)
 
-  | EType     of { name : Var.t   (** Type Alias *)
-                 ; typ  : Type.t
-                 ; body : t
-                 }
+  | EType of {
+      name : Var.t;
+      typ : Type.t;
+      body : t
+    } (** Type Alias *)
 
-  | EIf       of { guard : t   (** Conditional *)
-                 ; thenb : t
-                 ; elseb : t
-                 }
-  | EPrint    of t
+  | EIf of {
+      guard : t;
+      thenb : t;
+      elseb : t
+    } (** Conditional *)
+
+  | EPrint of t (** Print *)
